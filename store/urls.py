a0085b136526caaca974d030958e452feb85d0a2
satyajitgiram2000@gmail.com <satyajitgiram9545@gmail.com>
2024-03-19 15:00:19 +0530

    changed the webpage stylings and it's content

--- store/urls.py
+++ store/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import home, contact, shop
+from .views import home, contact, shop, about, product_detail
 
 urlpatterns = [
     path('', home, name='home'),
-    path('shop', shop, name="shop"),
+    path('products', shop, name="shop"),
     path('contact', contact, name='contact'),
+    path('about', about, name='about'),
+    path('product/<int:product_id>/', product_detail, name='product_detail'),
+
 ]
--- store/views.py
+++ store/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
-from .models import Banner, Category
+from .models import Banner, Category, Product
 from app.models import Testimonial
 
 def home(request):
     banners = Banner.objects.all()
     categories = Category.objects.all()
     testimonials = Testimonial.objects.all()
+    products = Product.objects.all()
+
     for testimonial in testimonials:
         testimonial.star_range = range(testimonial.star_rating)
-    return render(request, 'home.html', {'banners': banners, 'categories': categories, 'testimonials': testimonials})
+    return render(request, 'home.html', {'banners': banners, 'products': products, 'categories': categories, 'testimonials': testimonials})
 
 
 
@@ -16,4 +18,19 @@ def contact(request):
     return render(request, 'contact.html')
 
 def shop(request):
-    return render(request, 'shop.html')
+    products = Product.objects.all()
+    return render(request, 'shop.html', {'products': products})
+
+def about(request):
+    testimonials = Testimonial.objects.all()
+
+    return render(request, 'about.html', {'testimonials': testimonials})
+
+
+
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'product_detail.html', {'product': product})
