71402010c58d306cce1771317a31b3bee4684236
satyajitgiram2000@gmail.com <satyajitgiram9545@gmail.com>
2024-03-19 16:22:25 +0530

    modified the about us page content

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Banner, Category, Product
+from .models import Banner, Category, Product, Post
 
 admin.site.register(Banner)
 admin.site.register(Category)
